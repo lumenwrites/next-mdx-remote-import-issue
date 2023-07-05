9c5dc3d544e6b737972454bc65eb7899c95755fa
lumenwrites <lumenwrites@gmail.com>
2023-07-05 16:16:45 +0100

    Update.

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
 import { serialize } from 'next-mdx-remote/serialize'
-import { MDXRemote } from 'next-mdx-remote'
 
 async function main() {
   const source = 'Some **mdx** text, with a component <Test />'
